4172f46ed50b66257c9969e869f7954b9ff7733f
Luís Jesus <up202108683@edu.fe.up.pt>
2024-02-28 12:25:27 +0000

    added normal vectors on MyTriangle

--- tp3/MyTriangle.js
+++ tp3/MyTriangle.js
@@ -15,12 +15,25 @@ export class MyTriangle extends CGFobject {
         this.vertices = [
             -1, 1, 0,	//0
             -1, -1, 0,	//1
-            1, -1, 0		//2
+            1, -1, 0,		//2
+            -1, 1, 0, //3
+            -1, -1, 0, //4
+            1, -1, 0  //5
+        ];
+
+        this.normals = [
+            0, 0, 1, //0
+            0, 0, 1, //1
+            0, 0, 1, //2
+            0, 0, -1, //0
+            0, 0, -1, //1
+            0, 0, -1  //2
         ];
 
         //Counter-clockwise reference of vertices
         this.indices = [
-            0, 1, 2
+            0, 1, 2,
+            2, 1, 0
         ];
 
         //The defined indices (and corresponding vertices)

32b8aa536dd1cd78eae8bb460209da298464c14f
Luís Jesus <up202108683@edu.fe.up.pt>
2024-03-19 21:41:35 +0000

    Finished tp4

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -1,63 +1,79 @@
-import {CGFobject} from '../lib/CGF.js';
-import {MyQuad} from './MyQuad.js';
+import { CGFobject } from '../lib/CGF.js';
+import { MyQuad } from './MyQuad.js';
 
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
 export class MyUnitCubeQuad extends CGFobject {
-    constructor(scene) {
+    constructor(scene, textureTop, textureFront, textureRight, textureBack, textureLeft, textureBottom) {
         super(scene);
-        this.quad = new MyQuad(scene);
+        this.quad = new MyQuad(this.scene);
+        this.textureTop = textureTop;
+        this.textureFront = textureFront;
+        this.textureRight = textureRight;
+        this.textureBack = textureBack;
+        this.textureLeft = textureLeft;
+        this.textureBottom = textureBottom;
     }
 
     display() {
-        this.scene.setDiffuse(1.0, 1.0, 1.0, 0.5);
-        // Front face
-        this.scene.pushMatrix();
-        this.scene.setDiffuse(1.0, 1.0, 1.0, 0.5);
-        this.scene.translate(0, 0, 0.5);
-        this.quad.display();
-        this.scene.popMatrix();
-        
 
-        // Back face
-        this.scene.pushMatrix();
-        this.scene.setDiffuse(1.0, 1.0, 1.0, 0.5);
-        this.scene.rotate(Math.PI, 0, 1, 0);
-        this.scene.translate(0, 0, 0.5);
-        this.quad.display();
-        this.scene.popMatrix();
+        //Top
+        this.scene.pushMatrix()
+        this.scene.translate(0,0.5,0)
+        this.scene.rotate(3 * Math.PI/2 ,1, 0, 0)
+        this.textureTop.bind();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        this.quad.display()
+        this.scene.popMatrix()
 
+        //Front
+        this.scene.pushMatrix()
+        this.scene.translate(0.5, 0, 0)
+        this.scene.rotate(Math.PI / 2, 0, 1, 0)
+        this.textureFront.bind();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        this.quad.display()
+        this.scene.popMatrix()
         
-        // Left face
-        this.scene.pushMatrix();
-        this.scene.setDiffuse(1.0, 1.0, 1.0, 0.5);
-        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
-        this.scene.translate(0, 0, 0.5);
-        this.quad.display();
-        this.scene.popMatrix();
+        //Right
+        this.scene.pushMatrix()
+        this.scene.translate(0,0,-0.5)
+        this.scene.rotate(Math.PI,0, 1, 0)
+        this.textureRight.bind();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        this.quad.display()
+        this.scene.popMatrix()
         
-        // Right face
-        this.scene.pushMatrix();
-        this.scene.setDiffuse(1.0, 1.0, 1.0, 0.5);
-        this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        this.scene.translate(0, 0, 0.5);
-        this.quad.display();
-        this.scene.popMatrix();
+        //Back
+        this.scene.pushMatrix()
+        this.scene.translate(-0.5, 0, 0)
+        this.scene.rotate(3 * Math.PI / 2, 0, 1, 0)
+        this.textureBack.bind();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        this.quad.display()
+        this.scene.popMatrix()
+        
+        //Left
+        this.scene.pushMatrix()
+        this.scene.translate(0,0,0.5)
+        this.textureLeft.bind();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        this.quad.display()
+        this.scene.popMatrix()
 
-        // Top face
-        this.scene.pushMatrix();
-        this.scene.setDiffuse(1.0, 1.0, 1.0, 0.5);
-        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
-        this.scene.translate(0, 0, 0.5);
-        this.quad.display();
-        this.scene.popMatrix();
-
-        // Bottom face
-        this.scene.pushMatrix();
-        this.scene.setDiffuse(1.0, 1.0, 1.0, 0.5);
-        this.scene.rotate(Math.PI / 2, 1, 0, 0);
-        this.scene.translate(0, 0, 0.5);
-        this.quad.display();
-        
-        this.scene.popMatrix();
-        
+        //Bottom
+        this.scene.pushMatrix()
+        this.scene.translate(0,-0.5,0)
+        this.scene.rotate(Math.PI/2 ,1, 0, 0)
+        this.textureBottom.bind();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        this.quad.display()
+        this.scene.popMatrix()
+    
     }
+
+
 }

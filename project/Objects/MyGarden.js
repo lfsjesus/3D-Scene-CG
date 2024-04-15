da2e6c461260ed7ac55b9695399a0fb73e65c224
Miguel Rocha <up202108720@edu.fe.up.pt>
2024-04-15 19:43:12 +0100

    Fixed comments

--- project/Objects/MyGarden.js
+++ project/Objects/MyGarden.js
@@ -8,15 +8,16 @@ export class MyGarden extends CGFobject {
 
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < cols; j++) {
-                // Parâmetros da flor, ajuste conforme a necessidade
+                // Parâmetros da flor, temos de por random
                 let numPetals = 6;
                 let flowerRadius = 1;
                 let receptacleRadius = 0.3;
                 let stemRadius = 0.1;
                 let numStems = 3;
-                // Cores podem ser definidas aqui ou dentro da classe MyFlower
+                
+                //cores
                 let petalColor = [1.0, 0.0, 0.0, 1.0]; // Exemplo de cor vermelha
-                let recepColor, stemColor, leafColor; // Defina como desejar
+                let recepColor, stemColor, leafColor;
 
                 let flower = new MyFlower(
                     this.scene, numPetals, flowerRadius,
@@ -24,7 +25,7 @@ export class MyGarden extends CGFobject {
                     petalColor, recepColor, stemColor, leafColor
                 );
 
-                // Posição baseada no índice e no espaçamento entre as flores
+                // Posição baseada no índice e no espaço entre as flores
                 let xPosition = i * flowerSpacing;
                 let yPosition = j * flowerSpacing;
 
@@ -40,7 +41,7 @@ export class MyGarden extends CGFobject {
             let position = item.position;
 
             this.scene.pushMatrix();
-            this.scene.translate(position[0], 0, position[1]); // Ajuste a altura (eixo y) conforme necessário
+            this.scene.translate(position[0], 0, position[1]); //A altura é o eixo y neste caso
             flower.display();
             this.scene.popMatrix();
         }

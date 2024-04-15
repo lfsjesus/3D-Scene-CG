6d4a085a75b33dd62cc2ced6815d8be6979ee5d4
Miguel Rocha <up202108720@edu.fe.up.pt>
2024-04-15 18:45:25 +0100

    Added curved stems

--- project/Objects/MyFlower.js
+++ project/Objects/MyFlower.js
@@ -1,4 +1,5 @@
 import { CGFobject } from "../../lib/CGF.js";
+import { CGFappearance } from "../../lib/CGF.js";
 import { MyStem } from "./MyStem.js";
 import { MyPetal } from "./MyPetal.js";
 import { MyReceptacle } from "./MyReceptacle.js"
@@ -23,70 +24,89 @@ export class MyFlower extends CGFobject {
         }
 
         this.stems = [];
-
+        
         for (let i = 0; i < numStems; i++) {
 
-            //the height of each stem should be random between 1 and 2, inclusive
-            //let height = Math.random() + 1;
-            let height = 1;
+            //the height of each stem should be random between 0.5 and 2, inclusive
+            let height = Math.random() * 1 + 0.5;
             
             //the stems should have a random curvature angle between -PI/8 and PI/8
             let curvatureAngle = Math.random() * Math.PI/4 - Math.PI/8;
             this.stems.push(new MyStem(scene, 16, height, stemRadius, curvatureAngle));
+
+            //between each pair of stems, there should be added a new stem with a petal new MyStem(scene, 16, 0.1, radius, curvatureAngle, true)
+            if(i < numStems-1){
+                this.stems.push(new MyStem(scene, 16, 0.1, stemRadius, curvatureAngle, true));
+            }
+            
+
         }
         console.log(this.stems);
     }
 
     display() {
-        // Add stems
         let stemHeight = 0;
-        
+        let offsetZ = 0;
+        let lastCurvature = 0;
+    
         this.scene.pushMatrix();
-
+    
         for (let i = 0; i < this.stems.length; i++) {
             let stem = this.stems[i];
-
-            if (i > 0) { // Only translate if it's not the first stem
-                this.scene.translate(0, stem.height, 0);
-            }
-
-            // Always rotate
-            this.scene.rotate(stem.curvatureAngle, 1, 0, 0);
-            stem.display();
-            stemHeight += stem.height;
-        }
-
-        this.scene.popMatrix();
-
-
-        // TRANSLATE EVERYTHING UP TO THE TOP OF THE STEM
-        this.scene.pushMatrix();
-        this.scene.translate(0, stemHeight, 0);
-
-        // MyReceptacle
-        this.scene.pushMatrix();
-        this.receptacle.display();
-        this.scene.popMatrix();
-
-
-        // Add petals
-        this.scene.pushMatrix();
-        
-        for (let petal of this.petals) {
+            
 
             this.scene.pushMatrix();
-            //Rotate the petal by rotateAngle and heartAngle
-            this.scene.rotate(petal.heartAngle, 1, 0, 0);
+
+            //translate the stem to the correct height and offset
+            this.scene.translate(0, stemHeight, offsetZ);
+    
+           
+            this.scene.rotate(stem.curvatureAngle, 1, 0, 0);
+            let blueMaterial = new CGFappearance(this.scene);
+            if(stem.hasPetal){
+                //make the color of the stem blue
+                
+                blueMaterial.setAmbient(0, 0, 1, 1); // Blue color in RGBA
+                blueMaterial.apply();
+            }
+
+            // Draw the stem
+            stem.display();
+
+            //reset the material to the default
+            blueMaterial = new CGFappearance(this.scene);
+            blueMaterial.setAmbient(1, 1, 1, 1); // Default color in RGBA
+            blueMaterial.apply();
+    
+            this.scene.popMatrix();
+    
+            // Update stemHeight with the height of this stem to position the next one correctly
+            stemHeight += stem.height * Math.cos(stem.curvatureAngle);
+            offsetZ += stem.height * Math.sin(stem.curvatureAngle);
+            lastCurvature = stem.curvatureAngle;
+        }
+    
+        this.scene.popMatrix();
+    
+        // Now, translate everything up by the total height of the stems
+        this.scene.pushMatrix();
+        this.scene.translate(0, stemHeight, offsetZ);
+        this.scene.rotate(lastCurvature + 2, 1, 0, 0);
+    
+        // MyReceptacle
+        this.receptacle.display();
+    
+        // Add petals around the top of the stem
+        for (let petal of this.petals) {
+            this.scene.pushMatrix();
+            // Rotate the petal into position
             this.scene.rotate(petal.rotateAngle, 0, 0, 1);
+            this.scene.rotate(petal.heartAngle, 1, 0, 0);
             petal.display();
             this.scene.popMatrix();
         }
-
+    
         this.scene.popMatrix();
-
-        this.scene.popMatrix();
-        
-
-
     }
+    
 }
--- project/Objects/MyStem.js
+++ project/Objects/MyStem.js
@@ -2,10 +2,11 @@ import { CGFobject } from '../../lib/CGF.js';
 import { MyDisc } from '../Primitives/MyDisc.js';
 import { MyCylinder } from '../Primitives/MyCylinder.js';
 export class MyStem extends CGFobject {
-    constructor(scene, slices, height, radius, curvatureAngle) {
+    constructor(scene, slices, height, radius, curvatureAngle, hasPetal = false) {
         super(scene);
         this.height = height;
         this.radius = radius;
+        this.hasPetal = hasPetal;
         this.curvatureAngle = curvatureAngle;
         this.cylinder = new MyCylinder(scene, slices, height, radius); // The side surface
         this.topBase = new MyDisc(scene, slices, radius); // Top base disc
